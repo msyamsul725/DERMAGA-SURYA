7faa3e11bab1d618dc79e3fa63fa8957d17eb94e
msyamsul725 <msyamsul725@users.noreply.github.com>
2026-04-27 10:04:57 +0000

    deploy: a81e72b29f30ca60c2641e9158f8adcc380c81db

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3167543328" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4209126026" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
